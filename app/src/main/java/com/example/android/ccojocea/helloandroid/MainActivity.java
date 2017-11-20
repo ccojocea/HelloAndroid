9fbe77d079d9ba0cccda3cede82fe17bbbb7c53a
ccojocea <ccojocea@gmail.com>
2017-11-20 18:56:27 +0200

    Add layout for small screens

--- app/src/main/java/com/example/android/ccojocea/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/android/ccojocea/helloandroid/MainActivity.java
@@ -3,14 +3,24 @@ package com.example.android.ccojocea.helloandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public int SCREEN_WIDTH_PIXELS = 520;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        if (displayMetrics.widthPixels > SCREEN_WIDTH_PIXELS) {
+            setContentView(R.layout.activity_main);
+        } else {
+            setContentView(R.layout.activity_main_small);
+        }
     }
 }
